bb4b94d3fbb4cbf6b61b44fc4433c858b7982b4d
한상진 <s202188@naver.com>
2021-04-17 12:39:09 +0900

    Initial Commit

---
+++ ZeroWaste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZeroWaste
+//
+//  Created by 한상진 on 2021/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
