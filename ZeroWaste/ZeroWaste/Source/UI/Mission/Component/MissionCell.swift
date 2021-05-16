a18ef4d9ba1eff6c7cee36f4696199a1d15c3686
krgoodnews <krgoodnews@gmail.com>
2021-05-16 23:03:12 +0900

    feat: MissionCell 디자인 변경

--- ZeroWaste/ZeroWaste/Source/UI/Mission/Component/MissionCell.swift
+++ ZeroWaste/ZeroWaste/Source/UI/Mission/Component/MissionCell.swift
@@ -10,24 +10,46 @@ import SwiftUI
 struct MissionCell: View {
     var body: some View {
         VStack(alignment: .leading) {
-            HStack {
-                Image(systemName: "questionmark.folder.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 34, height: 34, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .padding(.init(8))
-                    .background(Color.gray)
-                    .clipShape(Circle())
+            HStack(alignment: .top) {
+                // MARK: 미션레벨
+                ZStack {
+                    Image("iconStar")
+                    Text("1")
+                        .bold()
+                        .foregroundColor(.white)
+                }
 
+                // MARK: 미션 주제 / 참가인원
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("{수돗물 끓여 마시기}")
+                        .bold()
+
+                    HStack {
+                        Image("iconPerson")
+
+                        Text("{푸른지구}")
+
+                        Divider()
+                            .frame(height: 10)
+
+                        Text("{2,431명}")
+                    }
+                }
                 Spacer()
-
-                Text("진행률")
             }
-            Spacer()
 
-            Text("미션명")
-                .bold()
-            Text("기간")
+            Spacer()
+                .frame(height: 24)
+
+            // MARK: 카테고리 / 좋아요
+            HStack(alignment: .bottom) {
+                Text("기간")
+                Spacer()
+                Image("iconHeart")
+                    .renderingMode(.template)
+                    .foregroundColor(.blue)
+                    .padding(.vertical, 6)
+            }
         }
         .padding()
         .background(Color(UIColor.systemGroupedBackground))
@@ -39,7 +61,7 @@ struct MissionCell_Previews: PreviewProvider {
     static var previews: some View {
 //        ForEach(ColorScheme.allCases, id: \.self) {
             MissionCell()
-                .previewLayout(.fixed(width: 339, height: 127))
+                .previewLayout(.sizeThatFits)
 //                .preferredColorScheme($0)
 //        }
 
