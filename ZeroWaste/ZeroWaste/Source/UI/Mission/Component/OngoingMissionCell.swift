b06103581253160f479e19c33df482a2d63fe088
krgoodnews <krgoodnews@gmail.com>
2021-06-03 23:41:09 +0900

    feat: xcodegen 설정 변경 및 StyleGuide 적용

--- ZeroWaste/ZeroWaste/Source/UI/Mission/Component/OngoingMissionCell.swift
+++ ZeroWaste/ZeroWaste/Source/UI/Mission/Component/OngoingMissionCell.swift
@@ -12,21 +12,18 @@ struct OngoingMissionCell: View {
         VStack(alignment: .leading) {
             HStack(alignment: .top) {
                 // MARK: 미션레벨
-                ZStack {
-                    Image("iconStar")
-                    Text("1")
-                        .bold()
-                        .foregroundColor(.white)
-                }
+                Image("iconLevel4")
 
                 // MARK: 미션 주제 / 참가인원
                 VStack(alignment: .leading, spacing: 8) {
                     Text("{수돗물 끓여 마시기}")
                         .font(.headline)
+                        .foregroundColor(.zWhite)
                         .lineLimit(1)
 
-                    Text("미션 인증까지 3일 남았어요")
+                    Text("{카페 / 230명 참여중}")
                         .font(.caption)
+                        .foregroundColor(.zWhite)
 
                 }
                 Spacer()
@@ -37,26 +34,26 @@ struct OngoingMissionCell: View {
 
             // MARK: 카테고리 / 좋아요
             HStack(alignment: .bottom) {
-                CategoryCell()
-                CategoryCell()
+                Text("00:00:00 남았어요!")
+                    .font(.caption)
+                    .foregroundColor(.zWhite)
                 Spacer()
 
                 Button(action: {}) {
                     Text("인증하기")
-                        .font(.subheadline)
+                        .font(.system(13, .bold))
                         .foregroundColor(.label)
                         .bold()
                         .padding(.vertical, 10)
-                        .padding(.horizontal, 24)
-                        .background(Color.green)
-                        .cornerRadius(8)
+                        .padding(.horizontal, 32)
+                        .background(Color.zAurora)
+                        .cornerRadius(18)
                 }
             }
         }
         .padding()
-        .background(Color(UIColor.systemGroupedBackground))
+        .background(Color.zBlack)
         .cornerRadius(16)
-        .shadow(radius: 10)
         
     }
 }
